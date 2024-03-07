bef555571ac35cb04cfaa16fb1eb23ac8885530d
StasNik <stas.nik12@gmail.com>
2024-03-07 12:37:34 +0300

    сверстал карту и информацию в футере

---
+++ assets/scripts/map.js
@@ -0,0 +1,19 @@
+const mapTitle = document.createElement('div'); mapTitle.className = 'gymn-map-title';
+const schMap = document.querySelector('.gymn-map');
+
+ mapTitle.textContent = 'Для активации карты нажмите по ней';
+ schMap.appendChild(mapTitle);
+
+ schMap.onclick = function() {
+     this.children[0].removeAttribute('style');
+     mapTitle.parentElement.removeChild(mapTitle);
+ }
+
+ schMap.onmousemove = function(event) {
+     mapTitle.style.display = 'block';
+     if(event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
+     if(event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
+ }
+ schMap.onmouseleave = function() {
+     mapTitle.style.display = 'none';
+ }
