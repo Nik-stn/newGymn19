3706e50fcd9c389527a07c3b98c373ef2881f6c3
StasNik <stas.nik12@gmail.com>
2024-03-11 11:01:39 +0300

    Добавил скролл на вверх

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,23 @@
-const myCarouselElement = document.querySelector("#myCarousel");
+window.addEventListener('scroll', () => {
+  const scrollUp = document.querySelector('.scrooll-to-top');
+  const scrollPosition = window.scrollY;
+  const windowWidth = window.innerWidth;
+  const headerFixed = document.querySelector('.header-nav');
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 3000,
-  touch: true,
-  pause: "hover",
-  wrap: true,
+  if (scrollPosition > 100 && windowWidth > 768) {
+    scrollUp.style.display = 'block';
+  } else {
+    scrollUp.style.display = 'none';
+  }
+
+  if (scrollPosition >= 200) {
+    headerFixed.classList.add('fixed');
+  } else {
+    headerFixed.classList.remove('fixed');
+  }
+});
+
+document.querySelector('.scrooll-to-top').addEventListener('click', (event) => {
+  event.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
