b544906ca633445eb537df243c2b1a51c3643b3b
KlinVid <stas.nik12@gmail.com>
2024-05-16 16:53:41 +0300

    Оптимизация кода верс для слаб

--- assets/scripts/vision.js
+++ assets/scripts/vision.js
@@ -1,106 +1,97 @@
-const siteFont = document.querySelectorAll(".site-content, .site-content h1, .site-content h2, .site-content h3, .site-content h4, .site-content h5, .site-content h6");
-const siteContent = document.querySelectorAll("button, a, li, h1, h2, h3, h4, h5 , h6, figcaption, footer, div, span, b, input");
-const siteBorder = document.querySelectorAll("div, article, hr");
-const zIndexFix = document.querySelectorAll('.header-nav, .privacy-policy-popup');
-const img = document.querySelectorAll("#ya-site-form0 input.ya-site-form__submit, img,  .gymn-map, .youtube, .ya-share2__list");
-const crossEyeSvg = document.getElementById("cross-eye");
-const normalEyeSvg = document.getElementById("normal-eye");
-const crossEyeWhiteSvg = document.getElementById("cross-eye-white");
-const homeSvg = document.querySelectorAll('.home');
-const homeWhiteSvg = document.querySelectorAll('.home-white');
-const xMarkSvg = document.querySelector('.x-mark');
-const xMarkWhiteSvg = document.querySelector('.x-mark-white');
-const menuBurgerSvg = document.querySelector('.menu-burger');
-const menuBurgerWhiteSvg = document.querySelector('.menu-burger-white');
-const chevronSvg = document.querySelectorAll('.mobile-chevron.black');
-const chevronWhiteSvg = document.querySelectorAll('.mobile-chevron.white');
-const siteBody = document.querySelector('body');
-const vision = document.querySelector(".vision");
-const siteBackground = document.querySelector(".site-content");
-const imgYes = document.getElementById("imagesYes");
-const letterSpacing = document.querySelectorAll(".changeLetterSpacing");
+const elements = {
+    siteFont: document.querySelectorAll(".site-content, .site-content h1, .site-content h2, .site-content h3, .site-content h4, .site-content h5, .site-content h6"),
+    siteContent: document.querySelectorAll("button, a, li, h1, h2, h3, h4, h5, h6, figcaption, footer, div, span, b, input"),
+    siteBorder: document.querySelectorAll("div, article, hr"),
+    zIndexFix: document.querySelectorAll('.header-nav, .privacy-policy-popup'),
+    img: document.querySelectorAll("#ya-site-form0 input.ya-site-form__submit, img, .gymn-map, .youtube, .ya-share2__list"),
+    crossEyeSvg: document.getElementById("cross-eye"),
+    normalEyeSvg: document.getElementById("normal-eye"),
+    crossEyeWhiteSvg: document.getElementById("cross-eye-white"),
+    homeSvg: document.querySelectorAll('.home'),
+    homeWhiteSvg: document.querySelectorAll('.home-white'),
+    xMarkSvg: document.querySelector('.x-mark'),
+    xMarkWhiteSvg: document.querySelector('.x-mark-white'),
+    menuBurgerSvg: document.querySelector('.menu-burger'),
+    menuBurgerWhiteSvg: document.querySelector('.menu-burger-white'),
+    chevronSvg: document.querySelectorAll('.mobile-chevron.black'),
+    chevronWhiteSvg: document.querySelectorAll('.mobile-chevron.white'),
+    siteBody: document.querySelector('body'),
+    vision: document.querySelector(".vision"),
+    siteBackground: document.querySelector(".site-content"),
+    imgYes: document.getElementById("imagesYes"),
+    letterSpacing: document.querySelectorAll(".changeLetterSpacing")
+};
 
 function updateStyles() {
-    siteBackground.style.background = "#fff";
-    siteContent.forEach(item => {
-        item.style.zIndex = 'unset';
-    });
-    zIndexFix.forEach(item => {
-        item.style.zIndex = '20';
-    });
-    [crossEyeSvg, ...chevronWhiteSvg].forEach(item => {
-        item.style.display = 'inline-block';
-    });
-    [vision, xMarkWhiteSvg, menuBurgerWhiteSvg, ...homeWhiteSvg].forEach(item => {
-        item.style.display = 'block';
-    });
-    [xMarkSvg, crossEyeWhiteSvg, normalEyeSvg, menuBurgerSvg, ...chevronSvg, ...homeSvg].forEach(item => {
-        item.style.display = 'none';
-    });
+    elements.siteBackground.style.background = "#fff";
+    elements.siteContent.forEach(item => item.style.zIndex = 'unset');
+    elements.zIndexFix.forEach(item => item.style.zIndex = '20');
+    [elements.crossEyeSvg, ...elements.chevronWhiteSvg].forEach(item => item.style.display = 'inline-block');
+    [elements.vision, elements.xMarkWhiteSvg, elements.menuBurgerWhiteSvg, ...elements.homeWhiteSvg].forEach(item => item.style.display = 'block');
+    [elements.xMarkSvg, elements.crossEyeWhiteSvg, elements.normalEyeSvg, elements.menuBurgerSvg, ...elements.chevronSvg, ...elements.homeSvg].forEach(item => item.style.display = 'none');
 }
 
 function applyStyles(borderColor, background, color) {
     updateStyles();
-    siteBorder.forEach(item => {
-      item.style.borderColor = borderColor;
+    elements.siteBorder.forEach(item => item.style.borderColor = borderColor);
+    elements.siteContent.forEach(item => {
+        item.style.background = background;
+        item.style.color = color;
     });
-    siteContent.forEach(item => {
-      item.style.background = background;
-      item.style.color = color;
-    });
-  }
+}
+
+function applyColorStyles(colorStyles) {
+    applyStyles(colorStyles.borderColor, colorStyles.background, colorStyles.color);
+}
+
+const styles = {
+    white: { borderColor: '#2E3332', background: '#fff', color: '#2E3332' },
+    blue: { borderColor: '#00f', background: '#9dd1ff', color: '#00f' },
+    black: { borderColor: '#fff', background: '#000', color: '#fff' },
+    beige: { borderColor: 'brown', background: '#f7f3d6', color: 'brown' }
+};
 
 function applyWhiteStyles() {
-    applyStyles('#2E3332', '#fff', '#2E3332');
-    chevronSvg.forEach(item => {
-        item.style.display = 'inline-block';
-    });
-    [xMarkWhiteSvg, menuBurgerWhiteSvg, ...homeWhiteSvg, ...chevronWhiteSvg].forEach(item => {
-        item.style.display = 'none';
-    });
-    [menuBurgerSvg, xMarkSvg, ...homeSvg].forEach(item => {
-        item.style.display = 'block';
-    });
+    applyColorStyles(styles.white);
+    elements.chevronSvg.forEach(item => item.style.display = 'inline-block');
+    [elements.xMarkWhiteSvg, elements.menuBurgerWhiteSvg, ...elements.homeWhiteSvg, ...elements.chevronWhiteSvg].forEach(item => item.style.display = 'none');
+    [elements.menuBurgerSvg, elements.xMarkSvg, ...elements.homeSvg].forEach(item => item.style.display = 'block');
 }
 
-function applyBlueStyles() {
-    applyStyles('#00f', '#9dd1ff', '#00f');
-}
-
-function applyBlackStyles() {
-    applyStyles('#fff', '#000', '#fff');
-    crossEyeSvg.style.display = "none";
-    crossEyeWhiteSvg.style.display = "block";
-}
-
-function applyBeigeStyles() {
-    applyStyles('brown', '#f7f3d6', 'brown');
+function applyColorScheme(colorScheme) {
+    switch (colorScheme) {
+        case 'blue':
+            applyColorStyles(styles.blue);
+            break;
+        case 'black':
+            applyColorStyles(styles.black);
+            elements.crossEyeSvg.style.display = "none";
+            elements.crossEyeWhiteSvg.style.display = "block";
+            break;
+        case 'beige':
+            applyColorStyles(styles.beige);
+            break;
+        default:
+            applyWhiteStyles();
+    }
 }
 
 function deleteVisionStyles() {
-    imgYes.dispatchEvent(new Event("click"));
-    siteContent.forEach(item => {
-        item.style.removeProperty("background");
-        item.style.removeProperty("color");
-        item.style.removeProperty("z-index");
-    });
-    [...chevronWhiteSvg, ...siteFont, ...img].forEach(item => item.removeAttribute("style"));
-    siteBorder.forEach(item => item.style.removeProperty("border-color"));
-    siteBody.removeAttribute("class");
-    [vision, crossEyeSvg, crossEyeWhiteSvg, xMarkSvg, menuBurgerSvg, ...chevronSvg, ...homeSvg].forEach(item => {
-        item.style.display = 'none';
-    });
-    [xMarkWhiteSvg, menuBurgerWhiteSvg, ...homeWhiteSvg].forEach(item => {
-        item.style.display = 'block';
-    });
-    normalEyeSvg.style.display = "inline-block";
+    elements.imgYes.dispatchEvent(new Event("click"));
+    elements.siteContent.forEach(item => item.removeAttribute("style"));
+    [...elements.chevronWhiteSvg, ...elements.siteFont, ...elements.img].forEach(item => item.removeAttribute("style"));
+    elements.siteBorder.forEach(item => item.style.removeProperty("border-color"));
+    elements.siteBody.removeAttribute("class");
+    [elements.vision, elements.crossEyeSvg, elements.crossEyeWhiteSvg, elements.xMarkSvg, elements.menuBurgerSvg, ...elements.chevronSvg, ...elements.homeSvg].forEach(item => item.style.display = 'none');
+    [elements.xMarkWhiteSvg, elements.menuBurgerWhiteSvg, ...elements.homeWhiteSvg].forEach(item => item.style.display = 'block');
+    elements.normalEyeSvg.style.display = "inline-block";
     localStorage.clear();
 }
 
 function normalEye(event) {
     event.preventDefault();
     applyWhiteStyles();
-    localStorage.setItem('applyWhiteStyles', applyWhiteStyles);
+    localStorage.setItem('applyWhiteStyles', 'true');
 }
 
 function crossEye(event) {
@@ -110,135 +101,87 @@ function crossEye(event) {
 
 function colorSiteClick(event) {
     event.preventDefault();
-    siteBody.removeAttribute("class");
-    let id = this.getAttribute("id");
-    siteBody.classList.add(id);
-    if (id === 'colorSiteBlue') {
-        applyBlueStyles();
-        localStorage.setItem('applyBlueStyles', applyBlueStyles);
-        ['applyBlackStyles', 'applyWhiteStyles', 'applyBeigeStyles'].forEach(key => localStorage.removeItem(key));
-    } else if (id === 'colorSiteBlack') {
-        applyBlackStyles();
-        localStorage.setItem('applyBlackStyles', applyBlackStyles);
-        ['applyBlueStyles', 'applyWhiteStyles', 'applyBeigeStyles'].forEach(key => localStorage.removeItem(key));
-    } else if (id === 'colorSiteWhite') {
-        applyWhiteStyles();
-        localStorage.setItem('applyWhiteStyles', applyWhiteStyles);
-        ['applyBlueStyles', 'applyBlackStyles', 'applyBeigeStyles'].forEach(key => localStorage.removeItem(key));
-    } else if (id === 'colorSiteBeige') {
-        applyBeigeStyles();
-        localStorage.setItem('applyBeigeStyles', applyBeigeStyles);
-        ['applyBlueStyles', 'applyBlackStyles', 'applyWhiteStyles'].forEach(key => localStorage.removeItem(key));
-    } else console.log('Error classlist');
+    elements.siteBody.removeAttribute("class");
+    const id = this.id.replace('colorSite', '').toLowerCase();
+    applyColorScheme(id);
+    localStorage.setItem(`apply${id.charAt(0).toUpperCase() + id.slice(1)}Styles`, 'true');
+    ['applyBlueStyles', 'applyBlackStyles', 'applyWhiteStyles', 'applyBeigeStyles'].forEach(key => key !== `apply${id.charAt(0).toUpperCase() + id.slice(1)}Styles` && localStorage.removeItem(key));
 }
 
 function updateFontSizes(changeAmount) {
-    let fz;
-    siteFont.forEach(item => {
-        fz = parseFloat(window.getComputedStyle(item).fontSize);
-        fz += changeAmount;
-        if (fz >= 12 && fz <= 24) item.style.fontSize = fz + "px";
-        else if (fz > 24) fz = 24;
-        else fz = 12;
+    elements.siteFont.forEach(item => {
+        const currentSize = parseFloat(window.getComputedStyle(item).fontSize);
+        const newSize = Math.min(Math.max(currentSize + changeAmount, 12), 24);
+        item.style.fontSize = `${newSize}px`;
     });
-    localStorage.setItem('applyFontSize', fz);
+}
+
+function updateImageDisplay(filter, display) {
+    elements.img.forEach(item => {
+        item.style.filter = filter;
+        item.style.display = display;
+    });
 }
 
 function imagesNoClick(event) {
     event.preventDefault();
-    const imgDelete = img.forEach(item => {
-        item.style.filter = 'grayscale(0%)';
-        item.style.display = "none";
-    });
-    localStorage.setItem('applyImgDelete', imgDelete);
+    updateImageDisplay('grayscale(0%)', 'none');
+    localStorage.setItem('applyImgDelete', 'true');
     ['applyImgColor', 'applyImgBlackWhiteStyles'].forEach(key => localStorage.removeItem(key));
 }
 
 function imagesYesClick(event) {
     event.preventDefault();
-    const imgColor = img.forEach(item => {
-        item.style.filter = 'grayscale(0%)';
-        item.style.display = "inline-block";
-    });
-    localStorage.setItem('applyImgColor', imgColor);
+    updateImageDisplay('grayscale(0%)', 'inline-block');
+    localStorage.setItem('applyImgColor', 'true');
     ['applyImgDelete', 'applyImgBlackWhiteStyles'].forEach(key => localStorage.removeItem(key));
 }
 
 function imagesBlackWhiteClick(event) {
     event.preventDefault();
-    const imgBlackWhite = img.forEach(item => {
-        item.style.filter = 'grayscale(100%)';
-        item.style.display = "inline-block";
-    });
-    localStorage.setItem('applyImgBlackWhiteStyles', imgBlackWhite);
+    updateImageDisplay('grayscale(100%)', 'inline-block');
+    localStorage.setItem('applyImgBlackWhiteStyles', 'true');
     ['applyImgColor', 'applyImgDelete'].forEach(key => localStorage.removeItem(key));
 }
 
 function spacing() {
     const data = this.getAttribute("data-letter-spacing");
-    siteFont.forEach(item => item.style.letterSpacing = data + 'px');
+    elements.siteFont.forEach(item => item.style.letterSpacing = `${data}px`);
     localStorage.setItem('applySpaceStyles', data);
 }
 
 function changeImageFontSpacing() {
-    siteFont.forEach(item => item.style.letterSpacing = getSpacing + 'px');
-    siteFont.forEach(item => item.style.fontSize = getFontSize + "px");
-    if (getImgColor !== null) {
-        img.forEach(item => {
-            item.style.filter = 'grayscale(0%)';
-            item.style.display = "inline-block";
-        });
-    } else if (getImgBlackWhite !== null) {
-        img.forEach(item => {
-            item.style.filter = 'grayscale(100%)';
-            item.style.display = "inline-block";
-        });
-    } else if (getImgDelete !== null) {
-        img.forEach(item => {
-            item.style.filter = 'grayscale(0%)';
-            item.style.display = "none";
-        });
+    elements.siteFont.forEach(item => {
+        item.style.letterSpacing = `${localStorage.getItem('applySpaceStyles')}px`;
+        item.style.fontSize = `${localStorage.getItem('applyFontSize')}px`;
+    });
+
+    if (localStorage.getItem('applyImgColor') !== null) {
+        updateImageDisplay('grayscale(0%)', 'inline-block');
+    } else if (localStorage.getItem('applyImgBlackWhiteStyles') !== null) {
+        updateImageDisplay('grayscale(100%)', 'inline-block');
+    } else if (localStorage.getItem('applyImgDelete') !== null) {
+        updateImageDisplay('grayscale(0%)', 'none');
     }
 }
 
-const getWhiteStyles = localStorage.getItem('applyWhiteStyles');
-const getBlackStyles = localStorage.getItem('applyBlackStyles');
-const getBlueStyles = localStorage.getItem('applyBlueStyles');
-const getBeigeStyles = localStorage.getItem('applyBeigeStyles');
-const getSpacing = localStorage.getItem('applySpaceStyles');
-const getFontSize = localStorage.getItem('applyFontSize');
-const getImgBlackWhite = localStorage.getItem('applyImgBlackWhiteStyles');
-const getImgDelete = localStorage.getItem('applyImgDelete');
-const getImgColor = localStorage.getItem('applyImgColor');
-
-if (getWhiteStyles !== null) {
-    applyWhiteStyles();
-    changeImageFontSpacing();
-} 
-else if (getBlackStyles !== null) {
-    applyBlackStyles();
-    changeImageFontSpacing();
-} 
-else if (getBlueStyles !== null) {
-    applyBlueStyles();
-    changeImageFontSpacing();
-} 
-else if (getBeigeStyles !== null) {
-    applyBeigeStyles();
-    changeImageFontSpacing();
-}
-
 function init() {
-    normalEyeSvg.addEventListener("click", normalEye);
-    crossEyeSvg.addEventListener("click", crossEye);
-    crossEyeWhiteSvg.addEventListener("click", crossEye);
+    elements.normalEyeSvg.addEventListener("click", normalEye);
+    elements.crossEyeSvg.addEventListener("click", crossEye);
+    elements.crossEyeWhiteSvg.addEventListener("click", crossEye);
     document.getElementById("reduceFont").addEventListener("click", () => updateFontSizes(-1));
     document.getElementById("increaseFont").addEventListener("click", () => updateFontSizes(1));
     document.querySelectorAll(".colorSite").forEach(item => item.addEventListener("click", colorSiteClick));
-    letterSpacing.forEach(item => item.addEventListener("click", spacing));
+    elements.letterSpacing.forEach(item => item.addEventListener("click", spacing));
     document.getElementById("imagesNo").addEventListener("click", imagesNoClick);
-    imgYes.addEventListener("click", imagesYesClick);
+    elements.imgYes.addEventListener("click", imagesYesClick);
     document.getElementById("imagesBlackWhite").addEventListener("click", imagesBlackWhiteClick);
 }
 
 init();
+
+const colorScheme = ['applyWhiteStyles', 'applyBlackStyles', 'applyBlueStyles', 'applyBeigeStyles'].find(key => localStorage.getItem(key));
+if (colorScheme) {
+    applyColorScheme(colorScheme.replace('apply', '').replace('Styles', '').toLowerCase());
+    changeImageFontSpacing();
+}
